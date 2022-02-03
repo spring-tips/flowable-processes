2314538662e3188f4031c3ae01509d67a9594980
Josh Long <josh@joshlong.com>
2022-02-02 23:46:54 -0800

    updated

--- src/main/java/com/example/flowable/FlowableApplication.java
+++ src/main/java/com/example/flowable/FlowableApplication.java
@@ -1,13 +1,8 @@
 package com.example.flowable;
 
 import lombok.extern.log4j.Log4j2;
-import org.flowable.engine.RuntimeService;
-import org.flowable.engine.TaskService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,23 +11,27 @@ import java.util.concurrent.atomic.AtomicInteger;
 @SpringBootApplication
 public class FlowableApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowableApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class, args);
+    }
 }
 
 @Log4j2
 @Service
 class EmailService {
 
-	ConcurrentHashMap<String, AtomicInteger> sends = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, AtomicInteger> sends = new ConcurrentHashMap<>();
 
-	public void sendWelcomeEmail(String customerId, String email) {
-		log.info("sending welcome email for " + customerId + " to " + email);
-		sends.computeIfAbsent(email, e -> new AtomicInteger());
-		sends.get(email).incrementAndGet();
+    AtomicInteger getSendCount(String key) {
+        return this.sends.get(key);
+    }
 
-	}
+    public void sendWelcomeEmail(String customerId, String email) {
+        log.info("sending welcome email for " + customerId + " to " + email);
+        sends.computeIfAbsent(email, e -> new AtomicInteger());
+        sends.get(email).incrementAndGet();
+
+    }
 
 
 }
--- src/test/java/com/example/flowable/FlowableApplicationTests.java
+++ src/test/java/com/example/flowable/FlowableApplicationTests.java
@@ -1,89 +1,61 @@
 package com.example.flowable;
 
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.flowable.engine.HistoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
-import org.flowable.engine.runtime.ProcessInstance;
-import org.flowable.task.api.Task;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
-@Log4j2
+@Slf4j
 public class FlowableApplicationTests {
 
 
-	private static String CUSTOMER_ID_PV = "customerId";
-	private static String EMAIL_PV = "email";
+    @Autowired
+    private HistoryService historyService;
 
-	@Autowired
-	private HistoryService historyService;
+    @Autowired
+    private RuntimeService runtimeService;
 
-	@Autowired
-	private RuntimeService runtimeService;
+    @Autowired
+    private EmailService emailService;
 
-	@Autowired
-	private EmailService emailService;
+    @Autowired
+    private TaskService taskService;
 
-	@Autowired
-	private TaskService taskService;
-
-	@Test
-	public void contextLoads() throws Exception {
-
-		String customerId = "1";
-		String email = "email@email.com";
-		String processInstanceId = this.beginCustomerEnrollmentProcess(customerId, email);
-		log.info("process instance ID: " + processInstanceId);
-		Assert.assertNotNull("the process instance ID should not be null", processInstanceId);
-
-		// get outstanding tasks
-
-		List<Task> tasks = this.taskService
-			.createTaskQuery()
-			.taskName("confirm-email-task")
-			.includeProcessVariables()
-			.processVariableValueEquals(CUSTOMER_ID_PV, customerId)
-			.list();
-
-		Assert.assertTrue("there should be one outstanding", tasks.size() >= 1);
-
-		// complete outstanding tasks
-
-		tasks.forEach(task -> {
-				this.taskService.claim(task.getId(), "jlong");
-				this.taskService.complete(task.getId());
-			}
-		);
-
-		// confirm that the email has been sent
-		Assert.assertEquals(this.emailService.sends.get(email).get(), 1);
-	}
+    @Test
+    public void contextLoads() throws Exception {
+        var customerId = "1";
+        var email = "email@email.com";
+        var processInstanceId = this.beginCustomerEnrollmentProcess(customerId, email);
+        log.info("process instance ID: " + processInstanceId);
+        Assertions.assertNotNull(processInstanceId, "the process instance ID should not be null");
+        var tasks = this.taskService
+                .createTaskQuery()
+                .taskName("confirm-email-task")
+                .includeProcessVariables()
+                .processVariableValueEquals("customerId", customerId)
+                .list();
+        Assertions.assertTrue(tasks.size() >= 1, "there should be one outstanding");
+        tasks.forEach(task -> {
+            this.taskService.claim(task.getId(), "jlong");
+            this.taskService.complete(task.getId());
+        });
+        Assertions.assertEquals(this.emailService.getSendCount(email).get(), 1);
+    }
 
 
-	String beginCustomerEnrollmentProcess(String customerId, String email) {
+    private String beginCustomerEnrollmentProcess(String customerId, String email) {
+        var vars = Map.of("customerId", (Object) customerId, "email", (Object) email);
+        var processInstance = this.runtimeService.startProcessInstanceByKey("signup-process", vars);
+        return processInstance.getId();
+    }
 
-		Map<String, Object> vars = new HashMap<>();
-		vars.put(CUSTOMER_ID_PV, customerId);
-		vars.put(EMAIL_PV, email);
-
-		ProcessInstance processInstance = this.runtimeService
-			.startProcessInstanceByKey("signup-process", vars);
-
-		return processInstance.getId();
-	}
-
-	void confirmEmail(String customerId) {
-	}
 
 }
